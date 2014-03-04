8b020a5d0f3ce259a43e34fe9dac19fad514731e
Dare Ryan <dareryan@me.com>
2014-03-03 20:09:20 -0500

    Add tutorial view

---
+++ BlackJack/BlackJack/HelpViewViewController.h
@@ -0,0 +1,14 @@
+//
+//  HelpViewViewController.h
+//  
+//
+//  Created by Dare Ryan on 3/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <FontAwesomeKit/FontAwesomeKit.h>
+
+@interface HelpViewViewController : UIViewController
+
+@end
